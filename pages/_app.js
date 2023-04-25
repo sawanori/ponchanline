18fd1234c4bdb19d5b1fcbf202e434374a2bf06c
noritakasawada <snp.inc.info@gmail.com>
2023-04-25 16:04:49 +0900

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
+import React, {useEffect} from 'react'
+import liff from "@line/liff"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+   const initializeLiff = async () => {
+      try {
+       await liff.init({ liffId: process.env.YOUR_LIFF_ID })
+      } catch (err) {
+        console.log("Error initializing LIFF: ", err)
+      }
+    }
+    initializeLiff()
+  }, [])
   return <Component {...pageProps} />
 }
 
